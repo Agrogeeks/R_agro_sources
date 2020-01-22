91efa8453f8032bb891dfbd79aa2d1892c7e65a2
juanchiem <edwardsmolina@gmail.com>
2020-01-22 08:48:41 -0300

    add example to readme

--- theme_weather.R
+++ theme_weather.R
@@ -7,11 +7,6 @@ theme_set(theme_bw() +
                   plot.title = element_text(size = 11, face = "bold"),
                   plot.caption = element_text(hjust=1)))
 
-# devtools::source_url("https://github.com/juanchiem/R-sources/blob/master/theme_weather.R?raw=TRUE")
-# myplot<-ggplot(data=data.frame(a=c(1,2,3), b=c(2,3,4)), aes(x=a, y=b)) + geom_line()
-# myplot
-# myplot + theme(panel.background = element_rect(fill = '#f7fcf9', colour = 'red'))
-
 # ht <- function(d, n=6){
 #   rbind(head(d, n, addrownums = TRUE))
 #   tail(d, n, addrownums = TRUE)
