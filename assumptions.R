879f395332678343bab4917a0f362502ef89de70
Juan Pablo Edwards Molina <edwardsmolina.juan@inta.gob.ar>
2020-02-13 09:57:48 -0300

    add lm assumptions file

---
+++ assumptions.R
@@ -0,0 +1,12 @@
+assump <- function(model)
+{
+  par(mfrow=c(3,1), 
+      oma = c(2, 2, 4, 2),
+      mar = c(4, 10, 4, 10)) 
+  plot(model, which=2, 
+       main = paste("P=", round(shapiro.test(model$residuals)$"p.value",3)))
+  plot(model, which=3, 
+       main=paste("P=",main= round(olsrr::ols_test_score(model)$p,3)))
+  MASS::boxcox(model)
+  layout(1)
+}
