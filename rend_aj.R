1b7eb11474f2d2e22540bf6e661cff55ade122eb
Juan Edwards Molina <edwardsmolina@gmail.com>
2020-02-17 14:56:35 -0300

    m_muestra

--- rend_aj.R
+++ rend_aj.R
@@ -1,12 +1,11 @@
 rinde_aj <- function( # kg/ha
-  surcos, 
-  largo,      # m
+  m_muestra,      # m
   dist_surcos, # cm
   h_muestra,     # g
   peso_tot,   # g
   h_deseada   # %
 ){ 
-  area_cosecha = surcos*largo*dist_surcos/100
+  area_cosecha = m_muestra*dist_surcos/100
   contenido_h = peso_tot*h_muestra/100
   rinde_s = ((peso_tot-contenido_h)/area_cosecha)*10 
   rinde_aj = round(rinde_s/(1-(h_deseada/100)), 0) 
@@ -14,8 +13,7 @@ rinde_aj <- function( # kg/ha
 }
 
 rinde_aj2 <- function( # kg/ha
-  surcos, 
-  largo,      # m
+  m_muestra      # m
   dist_surcos, # cm
   peso_h,     # g
   peso_s,     # g
@@ -23,7 +21,7 @@ rinde_aj2 <- function( # kg/ha
   peso_tot,   # g
   h_deseada   # %
 ){ 
-  area_cosecha = surcos*largo*dist_surcos/100
+  area_cosecha = m_muestra*dist_surcos/100
   
   h_muestra = (peso_h-(peso_s-tara_s))/peso_h
   
@@ -36,8 +34,8 @@ rinde_aj2 <- function( # kg/ha
   bind_rows(setNames(c(h_muestra*100, rinde_aj), 
                      c("hum", "rinde_aj")))
   }
-# rinde_aj2(surcos = 3,
-#          largo = 4 ,      # m
+# rinde_aj2(
+#          m_muestra = 12,      # m
 #          dist_surcos = 17.5, # cm
 #          peso_h = 48,     # g
 #          peso_s = 47,     # g
