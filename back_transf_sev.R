501ccafbdfc2cb4fa0d3088cccf791efb84ad3d4
Juan Edwards Molina <edwardsmolina@gmail.com>
2020-03-11 13:23:56 -0300

    emmeans way

--- back_transf_sev.R
+++ back_transf_sev.R
@@ -17,4 +17,22 @@ inv.arcsin <- function(x) {
 # dat <- data.frame(sev=c(0, 46.4, 69.5, 82.7, 61.7, 76.4, 84.8, 69.1,100))
 # dat$as <- asin(sqrt(dat$sev/100))
 # dat$as_back = zapsmall(inv.arcsin(dat$as))*100
-# plot(dat$sev, pch=3) ; points(dat$as_back, col="red")
+# par(mfrow=c(1,2))
+# plot(dat$sev, pch=3) ; points(dat$as_back, col="red"); plot(dat$as, pch=3) 
+
+# library(emmeans)
+# tran <- make.tran("asin.sqrt", 100)
+# my.model <- with(tran, 
+#                 lmer(linkfun(percent) ~ treatment + (1|Block), data = mydata))
+
+# warpbreaks %>% 
+#  group_by(wool, tension) %>% 
+#  summarise_all(mean)
+
+# bctran <- make.tran("boxcox", 0.368)
+# warp.bc <- with(bctran, 
+#                 lm(linkfun(breaks) ~ wool * tension, 
+#                    data = warpbreaks))
+# emmeans(warp.bc, ~ tension | wool, type = "response")
+
+
