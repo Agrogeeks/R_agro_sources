af03b6cc7700b8fe8ae8659c59e7186fa0e7ee34
Juan Edwards Molina <edwardsmolina@gmail.com>
2020-12-17 17:16:51 -0300

    Update lm_assumptions.R

--- lm_assumptions.R
+++ lm_assumptions.R
@@ -5,15 +5,15 @@ for (i in RequiredPackages) { #Installs packages if not yet installed
 
 assump <- function(model)
 {
-  par(mfrow=c(3,1), 
+  par(mfrow=c(2,1), 
       oma = c(2, 2, 4, 2),
       mar = c(4, 10, 4, 10)) 
   plot(model, which=2, 
        main = paste("P=", round(shapiro.test(model$residuals)$"p.value",3)))
   plot(model, which=3, 
        main=paste("P=",main= round(olsrr::ols_test_score(model)$p,3)))
-  bc <- MASS::boxcox(model)
-  lambda <- bc$x[which.max(bc$y)]
+#  bc <- MASS::boxcox(model)
+# lambda <- bc$x[which.max(bc$y)]
   layout(1)
-  print(paste("Box-Cox suggested transformation:", round(lambda, 1)))
+#  print(paste("Box-Cox suggested transformation:", round(lambda, 1)))
 }
